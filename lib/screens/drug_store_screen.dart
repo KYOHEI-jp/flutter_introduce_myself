b2d1f782029e752f9ab09d91413a2909051dd1e0
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-09-10 12:19:25 +0900

    Drawerの中身

---
+++ lib/screens/drug_store_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class DrugStoreScreen extends StatefulWidget {
+  const DrugStoreScreen({Key? key}) : super(key: key);
+
+  @override
+  _DrugStoreScreenState createState() => _DrugStoreScreenState();
+}
+
+class _DrugStoreScreenState extends State<DrugStoreScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/super_market_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SuperMarketScreen extends StatefulWidget {
+  const SuperMarketScreen({Key? key}) : super(key: key);
+
+  @override
+  _SuperMarketScreenState createState() => _SuperMarketScreenState();
+}
+
+class _SuperMarketScreenState extends State<SuperMarketScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
