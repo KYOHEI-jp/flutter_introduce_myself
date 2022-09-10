5cf911ffcc67d313f561b9070f22f3a9ad993e00
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-09-11 00:04:45 +0900

    ディスカウントストアスクリーン作成

---
+++ lib/projects/screens/discount_store_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DiscountStoreScreen extends StatelessWidget {
+  const DiscountStoreScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
