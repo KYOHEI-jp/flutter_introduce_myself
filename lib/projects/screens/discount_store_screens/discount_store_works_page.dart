386ffb3e8e8b496f20d9397eae62aeec1e5f95b7
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-09-20 23:48:24 +0900

    業務アプリ

---
+++ lib/projects/screens/discount_store_screens/discount_store_works_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import 'discount_store_screen.dart';
+
+class WorksPage extends StatelessWidget {
+  final Works works;
+
+  const WorksPage({Key? key, required this.works}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(works.workName),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.blue),
+              ),
+              child: Text(
+                "具体的な内容",
+                style: TextStyle(fontSize: 20.0),
+              ),
+              padding: EdgeInsets.all(8.0),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              child: Text(
+                works.content,
+                style: TextStyle(fontSize: 20.0),
+              ),
+              padding: EdgeInsets.all(15.0),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              decoration: BoxDecoration(border: Border.all(color: Colors.blue)),
+              child: Text(
+                "学んだ事",
+                style: TextStyle(fontSize: 20.0),
+              ),
+              padding: EdgeInsets.all(8.0),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              child: Text(
+                works.learned,
+                style: TextStyle(fontSize: 20.0),
+              ),
+              padding: EdgeInsets.all(15.0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
