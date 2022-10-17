1dd69e0ffed481a2b3b0f36e6438e27e6995d367
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-10-17 20:34:00 +0900

    delica_worksの中身

---
+++ lib/projects/screens/delica_screens/delica_works_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import 'delica_screen.dart';
+
+class WorksPage extends StatelessWidget {
+  final Works works;
+
+  const WorksPage({Key? key, required this.works}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(works.workName),
+        centerTitle: true,
+        backgroundColor: Colors.black38,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black38),
+              ),
+              child: Text(
+                "具体的な内容",
+                style: TextStyle(fontSize: 20.0),
+              ),
+              padding: EdgeInsets.all(8.0),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              child: Text(
+                works.content,
+                style: TextStyle(fontSize: 20.0),
+              ),
+              padding: EdgeInsets.all(15.0),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.black38)),
+              child: Text(
+                "学んだ事",
+                style: TextStyle(fontSize: 20.0),
+              ),
+              padding: EdgeInsets.all(8.0),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              child: Text(
+                works.learned,
+                style: TextStyle(fontSize: 20.0),
+              ),
+              padding: EdgeInsets.all(15.0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
